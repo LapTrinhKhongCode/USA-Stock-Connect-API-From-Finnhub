756c3d2cb6fc07fe608ac0c489d577b56212f2d9
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-06-25 18:42:43 +0700

    Add Market

---
+++ StockMarketSolution/Controllers/MarketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarketSolution.Controllers
+{
+	[Route("[controller]")]
+	public class MarketController : Controller
+	{
+		
+		[HttpGet]
+		
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
