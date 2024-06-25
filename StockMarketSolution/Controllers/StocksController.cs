34654aa8d6e54fd578c41a89b1dc435ec96604f5
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-06-25 22:05:07 +0700

    ADD DEMO V1

---
+++ StockMarketSolution/Controllers/StocksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarketSolution.Controllers
+{
+	[Route("[controller]")]
+	public class StocksController : Controller
+	{
+		
+		public async Task<IActionResult> Explore()
+		{
+			return View();
+		}
+	}
+}
