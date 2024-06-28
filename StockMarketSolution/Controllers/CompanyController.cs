61021bdbb11a6eeec34dcddb6cab5f624a24bf8e
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-06-28 16:37:07 +0700

    fix js

---
+++ StockMarketSolution/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarketSolution.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StockMarketSolution/Controllers/Education.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarketSolution.Controllers
+{
+    public class Education : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
